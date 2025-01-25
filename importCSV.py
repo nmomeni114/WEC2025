2e32658221c12693d00d95fe7781dbc33d8db5f9
Nima Momeni <nma75@sfu.ca>
2025-01-25 10:57:30 -0600

    made import_csv() function for use in main

--- importCSV.py
+++ importCSV.py
@@ -1,6 +1,8 @@
 import pandas as pd
 
-# Load border data
-border_data = pd.read_csv('border.csv')
-x_coords = border_data['x'].values
-y_coords = border_data['y'].values
+def import_csv(file_name):
+    # Load border data
+    border_data = pd.read_csv(file_name)
+    x_coords = border_data['x'].values
+    y_coords = border_data['y'].values
+    return border_data, x_coords, y_coords
