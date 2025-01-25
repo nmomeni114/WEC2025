08d52ee338b7f7ccf10bb7acc8933992db5b6c2b
Nima Momeni <nma75@sfu.ca>
2025-01-25 14:21:36 -0600

    added main.py

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-<<<<<<< Updated upstream
-=======
-import graphingFunctions as gf
-import matplotlib.pyplot as plt
->>>>>>> Stashed changes
 
 import graphingFunctions as gf
 import hexagon_utils as hex
@@ -12,14 +7,14 @@ s = 2.5
 
 #import border data and derivemax/min x/y values
 border_data_X, border_data_Y = gf.getCSV('coordinates1964.csv')
-<<<<<<< Updated upstream
 x_max, x_min, y_max, y_min = gf.findBorders(border_data_X, border_data_Y)
 
-hex_grid_x, hex_grid_y = hex.generate_hex_grid(x_min, x_max, y_min, y_max)
+hex_grid_x, hex_grid_y = hex.generate_hex_grid(x_min, x_max, y_min, y_max, s)
 
-=======
+hex_grid = [hex_grid_x,hex_grid_y]
+print(hex_grid)
+polygon = gf.getPolygonFromPoints(border_data_X, border_data_Y)
 
-polygonBorder = gf.getPolygonFromPoints(border_data_X, border_data_Y)
-x, y = polygonBorder.exterior.xy
-plt.plot(x, y, 'b-', label='Polygon Border')
->>>>>>> Stashed changes
+hex.greedy_hexagon_coverage(polygon.exterior.xy, hex_grid, s)
+
+#plt.plot(x, y, 'b-', label='Polygon Border')
