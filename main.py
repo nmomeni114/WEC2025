b54e9150aa88721ad1be1edad5a2881cbbe6609f
Nima Momeni <nma75@sfu.ca>
2025-01-25 13:46:57 -0600

    workin on main

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 import graphingFunctions as gf
+import hexagon_utils as hex
 
+#define side length of circumscribed hexagon (this is equal to the radius of the circle)
+s = 2.5
+
+#import border data and derivemax/min x/y values
 border_data_X, border_data_Y = gf.getCSV('coordinates1964.csv')
+x_max, x_min, y_max, y_min = gf.findBorders(border_data_X, border_data_Y)
+
+
+
+
