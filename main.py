5a4986dd12c417527d8d464a98803c94aaa374f1
SDcodeman <121375981+SDcodeman@users.noreply.github.com>
2025-01-25 11:54:37 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+<<<<<<< Updated upstream
+=======
+import graphingFunctions as gf
+import matplotlib.pyplot as plt
+>>>>>>> Stashed changes
 
 import graphingFunctions as gf
 import hexagon_utils as hex
@@ -7,8 +12,15 @@ s = 2.5
 
 #import border data and derivemax/min x/y values
 border_data_X, border_data_Y = gf.getCSV('coordinates1964.csv')
+<<<<<<< Updated upstream
 x_max, x_min, y_max, y_min = gf.findBorders(border_data_X, border_data_Y)
 
 
 
 
+=======
+
+polygonBorder = gf.getPolygonFromPoints(border_data_X, border_data_Y)
+x, y = polygonBorder.exterior.xy
+plt.plot(x, y, 'b-', label='Polygon Border')
+>>>>>>> Stashed changes
