5df432b898e9aa7880762ecb97d601e45d07d57c
Nima Momeni <nma75@sfu.ca>
2025-01-25 14:00:03 -0600

    added basics to make grid

--- main.py
+++ main.py
@@ -9,6 +9,5 @@ s = 2.5
 border_data_X, border_data_Y = gf.getCSV('coordinates1964.csv')
 x_max, x_min, y_max, y_min = gf.findBorders(border_data_X, border_data_Y)
 
-
-
+hex_grid_x, hex_grid_y = hex.generate_hex_grid(x_min, x_max, y_min, y_max)
 
